dce98f77aea2aad413ddd5c22d4855c887006e56
wanghuaijin <hjwang2334@163.com>
2024-08-29 05:12:11 +0800

    deploy

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 git add -A
 git commit -m "deploy"
-git push git@github.com:ciaotomorrow/ciaotomorrow.github.io.git main:main
+git push -f git@github.com:ciaotomorrow/ciaotomorrow.github.io.git main:main
